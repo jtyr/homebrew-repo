d345b5612eb5ea47c25df389e984cb517476078d
Jiri Tyr <jiri.tyr@gmail.com>
2017-11-24 21:45:14 +0000

    Version bump to v1.1.3

--- Formula/gbt.rb
+++ Formula/gbt.rb
@@ -1,10 +1,10 @@
 class Gbt < Formula
   desc "Highly configurable prompt builder for Bash and ZSH written in Go."
   homepage "https://github.com/jtyr/gbt"
-  version "1.1.2"
+  version "1.1.3"
   revision 1
-  url "https://github.com/jtyr/gbt/releases/download/v1.1.2/gbt-1.1.2-darwin-amd64.tar.gz"
-  sha256 "bb1fcbbacb8a6ad1c2e336bfc1e01aea30531e43915229010220d08d11e93ef8"
+  url "https://github.com/jtyr/gbt/releases/download/v1.1.3/gbt-1.1.3-darwin-amd64.tar.gz"
+  sha256 "55f41bb882408873274d78eca687e934b69f1714803ff61e23373a8bb4f12a67"
 
   def install
     bin.install "gbt"
