e67afd490d1830d4c5cab4cc0784b3a39f67779d
Jiri Tyr <jiri.tyr@gmail.com>
2018-09-03 21:27:07 +0100

    Adding gbt-git

---
+++ Formula/gbt-git.rb
@@ -0,0 +1,29 @@
+class GbtGit < Formula
+  desc "Highly configurable prompt builder for Bash and ZSH written in Go."
+  homepage "https://github.com/jtyr/gbt"
+  revision 1
+  head "https://github.com/jtyr/gbt.git"
+  sha256 ""
+  conflicts_with "gbt", :because => "because only one GBT can be installed"
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    dir = buildpath/"src/github.com/jtyr/gbt"
+    dir.install buildpath.children
+
+    cd dir do
+      system "go", "install", "./cmd/gbt"
+      bin.install buildpath/"bin/gbt"
+      doc.install "README.md"
+      doc.install "LICENSE"
+      pkgshare.install Dir["sources"]
+      pkgshare.install Dir["themes"]
+    end
+  end
+
+  test do
+    system "bin/gbt", "-version"
+  end
+end
--- Formula/gbt.rb
+++ Formula/gbt.rb
@@ -2,9 +2,10 @@ class Gbt < Formula
   desc "Highly configurable prompt builder for Bash and ZSH written in Go."
   homepage "https://github.com/jtyr/gbt"
   version "1.2.0"
-  revision 2
+  revision 3
   url "https://github.com/jtyr/gbt/releases/download/v1.2.0/gbt-1.2.0-darwin-amd64.tar.gz"
   sha256 "6b626d4da6344d6db36647d44d505faf722b755543c0882f78a2b28a00b05d28"
+  conflicts_with "gbt-git", :because => "only one GBT can be installed"
 
   def install
     bin.install "gbt"
